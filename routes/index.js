15571428f1db5ab8414b501bc60b496517e36398
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2020-01-15 15:51:28 +0000

    User can submit VRM and it renders in the view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'VLU' });
+  res.render('index', { title: 'VLU', vrms : vrms});
 });
+
+
+var vrms = [];
+
+/* POST search new VRM. */
+router.post('/vrms', function(req, res, next) {
+    var query = { vrm: req.body.vrm.replace(/\s+/g, '').toUpperCase() };
+    console.log(query);
+    vrms.push(query);
+    console.log(vrms)
+    res.status(201).redirect('/');
+  });
+
+/* GET vrms listing. */
+router.get('/', function(req, res, next) {
+  res.render('vrms/index', { vrms : vrms });
+});
+
 
 module.exports = router;
